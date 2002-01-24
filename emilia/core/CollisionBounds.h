7a31e05158e55bca1c70489eb27684afdf9ded13
Henrik Enqvist <henqvist@users.sf.net>
2002-01-24 07:01:59 +0000

    no message

---
+++ emilia/core/CollisionBounds.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+                          CollisionBounds.h  -  description
+                             -------------------
+    begin                : Sun Mar 19 2000
+    copyright            : (C) 2000 by 
+    email                : 
+ ***************************************************************************/
+
+#ifndef COLLISIONBOUNDS_H
+#define COLLISIONBOUNDS_H
+
+#include "Private.h"
+
+#include "EMath.h"
+#include "Node.h"
+
+class Shape3D;
+class Polygon;
+class Group;
+
+/**
+ *
+ */
+class CollisionBounds {
+	public:
+		/** <p>CollisionBounds( radius, x, y, z ).
+		 * radius is the size of the bounds x y z is center of the bounds
+		 * relative to the center of the group.</p>*/
+		CollisionBounds(float size, float x=0.0f, float y=0.0f, float z=0.0f);
+		~CollisionBounds();
+		
+		/** <p>Add all polygons in Shape3D to bounds</p>*/
+		void setShape3D(Shape3D * s, int level);
+		void transform(const Matrix & mtx);
+		
+	private:
+		friend class Group;
+		friend class CollisionVisitor;
+		friend class BoundsRenderVisitor;
+		friend class AlignVisitor;
+
+		/** <p>Split the polygons in the collsion bound into 8 smaller collision
+		 * bounds. The new collision will be located at x = oldX +/- 0.25*oldSize,
+		 * y = oldY +/- 0.25*oldSize and z = oldZ + +/- 0.25*oldSize and the new size
+		 * will be 0.5*oldSize.</p>*/
+		void split(int level);
+		int intersect(Polygon* p);
+		void setParent(Group* g);
+		int surround(Polygon* p);
+		/** <p>Removes empty bounding boxes from the tree fo bounding
+			boxes.</p>*/
+		void removeEmpty();
+		void add(Polygon* p, int, bool*);
+		void printTree(int level=0);
+		
+		Vertex3D m_vtxSrc;
+		Vertex3D m_vtxTrans;
+			
+		Group* p_Parent;
+		float m_fBoxSize;
+		float m_fRadius;
+		vector<Polygon*> m_vPolygon;
+		vector<CollisionBounds*> m_vCollisionBounds;
+//		float m_fR, m_fG, m_fB;
+};
+
+#endif // COLLISIONBOUNDS_H
