ecbc62261c7780605efb59bee32ecd2fcf736cbb
Henrik Enqvist <henqvist@users.sf.net>
2002-02-13 13:35:00 +0000

    *** empty log message ***

--- test/billboard.cpp
+++ test/billboard.cpp
@@ -8,6 +8,7 @@
 #include "KeyBehavior.h"
 #include "Keyboard.h"
 #include "TextureUtil.h"
+#include "Cube.h"
 
 /** Main */
 int main(int argc, char *argv[]) {
@@ -30,13 +31,19 @@ int main(int argc, char *argv[]) {
 	groupCamera->setTranslation(0, 0, 3);
 	engine->setEngineCamera(groupCamera);
 
+	// Add a cube
+	Cube* cube = new Cube(0.5, 1, 1, 0, 1);
+	Group* groupC = new Group();
+	groupC->addShape3D(cube);
+	engine->add(groupC);
+
 	// Load a texture
 	EmTexture* texture = TextureUtil::loadTexture(argv[1]);
 
 	// Add a BillBoard
 	BillBoard* billboard = new BillBoard(texture, 1.0, 1.0);
 	billboard->setProperty(EM_BILLBOARD_ALPHATEST);
-	billboard->setProperty(EM_BILLBOARD_TRANS);
+	//	billboard->setProperty(EM_BILLBOARD_TRANS);
 	Group* groupBB = new Group();
 	engine->add(groupBB);
 	groupBB->setBillBoard(billboard);
