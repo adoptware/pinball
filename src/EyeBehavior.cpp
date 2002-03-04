242e417e354c67cafaf2cc1081bccf80fe1a1a5c
Henrik Enqvist <henqvist@users.sf.net>
2002-03-04 14:53:02 +0000

    *** empty log message ***

--- src/EyeBehavior.cpp
+++ src/EyeBehavior.cpp
@@ -10,6 +10,8 @@
 #include "EyeBehavior.h"
 #include "Group.h"
 #include "Pinball.h"
+#include "Config.h"
+#include "Score.h"
 
 #define TX 0 
 #define TY 30
@@ -18,19 +20,65 @@
 #define RY 0
 #define RZ 0
 
-EyeBehavior::EyeBehavior(Group * g) : Behavior() {
-	m_gBall = g;
+EyeBehavior::EyeBehavior(Group * g1, Group * g2, Group * g3) : Behavior() {
+	p_gBall1 = g1;
+	p_gBall2 = g2;
+	p_gBall3 = g3;
+	p_Score = Score::getInstance();
 }
 
 EyeBehavior::~EyeBehavior() {
 }
 
 void EyeBehavior::onTick() {
-	EmAssert(m_gBall != NULL, "Ball group NULL");
+	EmAssert(p_gBall1 != NULL, "Ball1 group NULL");
+	EmAssert(p_gBall2 != NULL, "Ball2 group NULL");
+	EmAssert(p_gBall3 != NULL, "Ball3 group NULL");
 	EmAssert(p_Parent != NULL, "Parent group NULL");
 
-	float bx, by, bz, ex, ey, ez, sx, sy, sz;
-	m_gBall->getTranslation(bx, by, bz);
+	if (Config::getInstance()->getView() == 1) {
+		p_Parent->setTransform(0, 25, 25, 0.175f, 0, 0);
+		return;
+	}
+
+	float bx=0, by=0, bz=0;
+	int balls = 0;
+	float ex, ey, ez, sx, sy, sz;
+
+	if (p_Score->isBallActive(PBL_BALL_1)) {
+		float x, y, z;
+		p_gBall1->getTranslation(x, y, z);
+		bx = x; by = y; bz = z;
+		balls++;
+	}
+	if (p_Score->isBallActive(PBL_BALL_2)) {
+		float x, y, z;
+		p_gBall2->getTranslation(x, y, z);
+		if (balls > 0) {
+			bx += x; 
+			by = EM_MAX(by, y); 
+			bz = EM_MAX(bz, z);
+		} else {
+			bx = x; by = y; bz = z;
+		}
+		balls++;
+	}
+	if (p_Score->isBallActive(PBL_BALL_3)) {
+		float x, y, z;
+		p_gBall3->getTranslation(x, y, z);
+		if (balls > 0) {
+			bx += x; 
+			by = EM_MAX(by, y); 
+			bz = EM_MAX(bz, z);
+		} else {
+			bx = x; by = y; bz = z;
+		}
+		balls++;
+	}
+	if (balls > 0) {
+		bx /= balls;
+	}
+
 	p_Parent->getTranslation(ex, ey, ez);
 
 	sx = (TX+bx*0.4) - ex;
