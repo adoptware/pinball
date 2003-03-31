b6dc46f8d48063797ecfed65a7d3076b13ce6d3c
Henrik Enqvist <henqvist@users.sf.net>
2003-03-31 05:57:08 +0000

    *** empty log message ***

--- data/professor/ModuleProfessor.cpp
+++ data/professor/ModuleProfessor.cpp
@@ -12,99 +12,100 @@
 #include "Pinball.h"
 #include "Loader.h"
 #include "StateMachine.h"
-#include "Score.h"
+#include "Table.h"
 #include "Keyboard.h"
+#include "Score.h"
+#include "Config.h"
 
 class ProfessorBehavior : public Behavior {
 public:
-	ProfessorBehavior() : Behavior() {
-		Loader * loader = Loader::getInstance();
-		// init signals
-		//m_sigRightLoop = loader->getSignal("rightloop");
-		this->clear();
-	};
-	~ProfessorBehavior() {};
-
-	void onTick() {
-		// launch ball
-		if (Score::getInstance()->active() == 0 && 
-				Score::getInstance()->getCurrentBall() < 4 
-				&& Keyboard::isKeyDown(SDLK_RETURN)) {
-			switch (Score::getInstance()->getCurrentBall()) {
-			case 1 :
-				if (Score::getInstance()->isBallDead(PBL_BALL_1) ) {
-					SendSignal( PBL_SIG_BALL1_ON, 0, this->getParent(), NULL );
-					Score::getInstance()->activateBall(PBL_BALL_1);	
-					Score::getInstance()->clearText();
-					break;
-				}	
-			case 2 :
-				if (Score::getInstance()->isBallDead(PBL_BALL_2)) {
-					SendSignal( PBL_SIG_BALL2_ON, 0, this->getParent(), NULL );
-					Score::getInstance()->activateBall(PBL_BALL_2);
-					Score::getInstance()->clearText();
-					break;
-				}
-			case 3 :
-				if (Score::getInstance()->isBallDead(PBL_BALL_3)) {
-					SendSignal( PBL_SIG_BALL3_ON, 0, this->getParent(), NULL );
-					Score::getInstance()->activateBall(PBL_BALL_3);
-					Score::getInstance()->clearText();
-					break;
-				}
-				if (Score::getInstance()->isBallDead(PBL_BALL_1) ) {
-					SendSignal( PBL_SIG_BALL1_ON, 0, this->getParent(), NULL );
-					Score::getInstance()->activateBall(PBL_BALL_1);	
-					Score::getInstance()->clearText();
-					break;
-				}	
-				if (Score::getInstance()->isBallDead(PBL_BALL_2)) {
-					SendSignal( PBL_SIG_BALL2_ON, 0, this->getParent(), NULL );
-					Score::getInstance()->activateBall(PBL_BALL_2);
-					Score::getInstance()->clearText();
-					break;
-				}
-			default:
-				throw string("all balls busy");
-			}
-			EM_COUT("Score::onTick() new ball", 1);
-		}
-	};
-
-	void StdOnCollision() {};
-
-	void StdOnSignal() {
-		//EM_COUT((int)em_signal, 1);
-		
-		OnSignal( PBL_SIG_RESET_ALL ) {
-			this->clear();
-		} else
-		// ball dead
-		OnSignal( PBL_SIG_BALL1_OFF OR_SI 
-							PBL_SIG_BALL2_OFF OR_SI	
-							PBL_SIG_BALL3_OFF OR_SI 
-							PBL_SIG_BALL4_OFF ) {
-			if (Score::getInstance()->active() == 1) {
-				SendSignal( PBL_SIG_MULTIBALL_OFF, 0, this->getParent(), NULL );
-			}
-			if (Score::getInstance()->active() == 0) {
-				if (Score::getInstance()->getCurrentBall() < 3) {
-					Score::getInstance()->setCurrentBall(Score::getInstance()->getCurrentBall()+1);
-				} else {
-					SendSignal( PBL_SIG_GAME_OVER, 0, this->getParent(), NULL );
-				}
-			}
-		}
+  ProfessorBehavior() : Behavior() {
+    this->clear();
+  };
+  ~ProfessorBehavior() {};
+  
+  void onTick() {
+    Table * table = Table::getInstance();
+    Score * score = table->getScore();
+    EmAssert(score != NULL, "ProfessorBehavior::onTick socre NULL");
+    // launch ball
+    string launch("launch");
+    if (table->active() == 0 && 
+	table->getCurrentBall() < 4 
+	&& Keyboard::isKeyDown(Config::getInstance()->getKey(launch))) {
+      switch (table->getCurrentBall()) {
+      case 0 :
+	if (table->isBallDead(0) ) {
+	  SendSignal( PBL_SIG_BALL_ON, 0, this->getParent(), NULL );
+	  table->activateBall(0);	
+	  //score->clearText();
+	  break;
+	}	
+      case 1 :
+	if (table->isBallDead(1)) {
+	  SendSignal( PBL_SIG_BALL_ON, 0, this->getParent(), NULL );
+	  table->activateBall(1);
+	  //score->clearText();
+	  break;
 	}
-
-	void clear() {
-	};
-
+      case 2 :
+	if (table->isBallDead(2)) {
+	  SendSignal( PBL_SIG_BALL_ON, 0, this->getParent(), NULL );
+	  table->activateBall(2);
+	  //score->clearText();
+	  break;
+	}
+	if (table->isBallDead(0)) {
+	  SendSignal( PBL_SIG_BALL_ON, 0, this->getParent(), NULL );
+	  table->activateBall(0);	
+	  //score->clearText();
+	  break;
+	}	
+	if (table->isBallDead(1)) {
+	  SendSignal( PBL_SIG_BALL_ON, 0, this->getParent(), NULL );
+	  table->activateBall(1);
+	  //score->clearText();
+	  break;
+	}
+      default:
+	throw string("all balls busy");
+      }
+      EM_COUT("Table::onTick() new ball", 1);
+    }
+  };
+  
+  void StdOnCollision() {};
+  
+  void StdOnSignal() {
+    //EM_COUT((int)em_signal, 1);
+    Table * table = Table::getInstance();
+    
+    OnSignal( PBL_SIG_RESET_ALL ) {
+      this->clear();
+    } else
+      // ball dead
+      OnSignal( PBL_SIG_BALL_OFF ) {
+      if (table->active() == 1) {
+	SendSignal( PBL_SIG_MULTIBALL_OFF, 0, this->getParent(), NULL );
+      }
+      if (table->active() == 0) {
+	if (table->getCurrentBall() < 3) {
+	  table->setCurrentBall(table->getCurrentBall()+1);
+	} else {
+	  SendSignal( PBL_SIG_GAME_OVER, 0, this->getParent(), NULL );
+	}
+      }
+    }
+  }
+  
+  void clear() {
+  };
+  
 private:
-
+  
 };
 
 extern "C"  void * new_object_fct(void) {
-	return new ProfessorBehavior();
+  return new ProfessorBehavior();
 }
 
