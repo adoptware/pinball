71911cb0367845f385a115dbdb109c28e4ddcba5
henqvist <henqvist>
2003-04-25 05:57:11 +0000

    more faces to cone and cylinder

--- addon/Cylinder.cpp
+++ addon/Cylinder.cpp
@@ -10,57 +10,48 @@
 #include "Cylinder.h"
 #include "Polygon.h"
 
-Cylinder::Cylinder(float fSize, float fR, float fG, float fB, float fA, bool bTop) : Shape3D(8+8, 8+2) {
+Cylinder::Cylinder(float fSize, int sides, float fR, float fG, float fB, float fA, bool bTop) : Shape3D(sides+sides, sides+2) {
+	if (sides < 3) sides = 3;
 	Polygon3D * p;
 	// First ring of vertices.
-	{ for (float a=0; a<8; a+=1) {
-		this->add(EMath::emSin(a/8)*fSize/2, (float)fSize/2 , EMath::emCos(a/8)*fSize/2,
+	{ for (float a=0; a<sides; ++a) {
+		this->add(EMath::emSin(a/sides)*fSize/2, (float)fSize/2 , EMath::emCos(a/sides)*fSize/2,
 							fR, fG, fB, fA, 0.0f, 0.0f);
 	} }	
 	// Second ring.
-	{ for (float a=0; a<8; a+=1) {
-		this->add(EMath::emSin(a/8)*fSize/2, (float)-fSize/2, EMath::emCos(a/8)*fSize/2,
+	{ for (float a=0; a<sides; ++a) {
+		this->add(EMath::emSin(a/sides)*fSize/2, (float)-fSize/2, EMath::emCos(a/sides)*fSize/2,
 							fR, fG, fB, fA, 0.0f, 0.0f);
 	} }
 	// Add vertices.
 	// The ring of polygons.
-	{ for (int a=0; a<7; a++) {
+	{ for (int a=0; a<sides-1; ++a) {
 		p = new Polygon3D(this, 4);
 		p->add(a);
 		p->add(a+1);
-		p->add(a+9);
-		p->add(a+8);
+		p->add(a+sides+1);
+		p->add(a+sides);
 		this->add(p);
 	} }
 
 	p = new Polygon3D(this, 4);
-	p->add(7);
+	p->add(sides-1);
 	p->add(0);
-	p->add(8);
-	p->add(15);
+	p->add(sides);
+	p->add(sides+sides-1);
 	this->add(p);
 	// Top
 	if (bTop) {
-		p = new Polygon3D(this, 8);
-		p->add(7);
-		p->add(6);
-		p->add(5);
-		p->add(4);
-		p->add(3);
-		p->add(2);
-		p->add(1);
-		p->add(0);
+		p = new Polygon3D(this, sides);
+		for (int a=sides-1; a>=0; --a) {
+			p->add(a);
+		}
 		this->add(p);
 		// Bottom
-		p = new Polygon3D(this, 8);
-		p->add(8);
-		p->add(9);
-		p->add(10);
-		p->add(11);
-		p->add(12);
-		p->add(13);
-		p->add(14);
-		p->add(15);
+		p = new Polygon3D(this, sides);
+		for (int a=sides; a<sides+sides; ++a) {
+			p->add(a);
+		}
 		this->add(p);
 	}
 
