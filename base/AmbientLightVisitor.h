e918d321063de080aa79152f8016e7789f70ff28
henqvist <henqvist>
2003-05-26 08:41:12 +0000

    Moved code from PointLightVisitor to AmbientLightVisitor. Added EM_USE_DIFFUSE, EM_USE_SPECULAR properties to lights. Fixed no-lights config option.

--- base/AmbientLightVisitor.h
+++ base/AmbientLightVisitor.h
@@ -21,18 +21,22 @@ class Shape3D;
 /** Only for internal use. */
 class AmbientLightVisitor : public Visitor {
  protected:
-	AmbientLightVisitor();
+  AmbientLightVisitor(int size = 4);
  public:
-	~AmbientLightVisitor();
-	static AmbientLightVisitor * getInstance();
-	void setLightning(float s, float bg);
-	void add(Light* l);
-	void visit(Group* g);
+  ~AmbientLightVisitor();
+  static AmbientLightVisitor * getInstance();
+  void setLightning(float s, float bg);
+  void add(Light* l);
+  void visit(Group* g);
+  void clear();
  private:
-	vector<Light*> m_vLight;
-	float m_fStrength;
-	float m_fBackground;
-	static AmbientLightVisitor * p_AmbientLightVisitor;
+  void visitAmbient(Shape3D* s);
+  void visitPoint(Shape3D* s);
+  
+  vector<Light*> m_vLight;
+  float m_fStrength;
+  float m_fBackground;
+  static AmbientLightVisitor * p_AmbientLightVisitor;
 };
 
-#endif // POINTLIGHTVISITOR_H
+#endif // AMBIENTLIGHTVISITOR_H
