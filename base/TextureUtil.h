7b23958ff22040bd5f0a3589ef7fbec171ace30f
Philippe Coval <rzr@users.sf.net>
2003-06-13 13:39:34 +0000

    win32 resize bug hidden

--- base/TextureUtil.h
+++ base/TextureUtil.h
@@ -1,11 +1,11 @@
-//#ident "$Id: TextureUtil.h,v 1.4 2003/05/12 12:17:59 rzr Exp $"
+//#ident "$Id: TextureUtil.h,v 1.5 2003/06/13 13:39:46 rzr Exp $"
 /***************************************************************************
                           TextureUtil.h  -  description
                              -------------------
     begin                : Mon Nov 27 2000
     copyright            : (C) 2000 by Henrik Enqvist
     email                : henqvist@excite.com
- ***************************************************************************/
+***************************************************************************/
 
 
 #ifndef TEXTUREUTIL_H
@@ -16,8 +16,7 @@
 #endif
 
 #if EM_USE_SDL
-//#include <GL/gl.h>
-#include <SDL_opengl.h> //!+-rzr: better portability
+#include <SDL_opengl.h>
 
 typedef struct {
   unsigned int width;
@@ -46,27 +45,31 @@ extern ZBUFFER * zbuffer;
 
 #include <map>
 #include <string>
+#include <list>
 
 #include "EMath.h"
 
 /** Singleton class for initializing graphics and loading textures. */
 class TextureUtil {
- protected:
+protected:
   TextureUtil();
- public:
+public:
   ~TextureUtil();
   static TextureUtil* getInstance();
   void initGrx();
   void stopGrx();
-  // TODO 
   /// just unload the textures from main mem (needed w32)
   void freeTextures(); 
-  void resizeView(unsigned int w, unsigned int h);
+  void reloadTextures();
+  void resizeView(unsigned int w = 320 , unsigned int h = 240 );
   EmTexture* loadTexture(const char* file);
   EmImage* loadImage(const char* file);
   const char * getTextureName(EmTexture * tex);
-  void setClearColor(float r, float g, float b, float a);
- private:
+  void setClearColor(float r = 1, float g = 1, float b = 1, float a = 1);
+  void getFilename(list<string>& filenames);
+private:
+  /// load it also in openGL context
+  int genTexture( char const * const filename, EmTexture * const texture);
   map<EmTexture*, string> m_hImageName;
   map<string, EmTexture*> m_hEmTexture;
   static TextureUtil* p_TextureUtil;
@@ -74,3 +77,4 @@ class TextureUtil {
 };
 
 #endif // TEXTUREUTIL_H
+//EOF: $Id: TextureUtil.h,v 1.5 2003/06/13 13:39:46 rzr Exp $
