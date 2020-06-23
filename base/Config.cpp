be01bd5891b12af6e2a31ec692bf0eb8ef2a1e1b
Philippe Coval <rzr@users.sf.net>
2020-06-23 19:38:31 +0200

    fix constant.
    Change-Id: Id061c6e664c999fe7d2214f5ee5c535af7c6cb66
    Origin: https://sources.debian.org/data/main/p/pinball/0.3.1-14.1/debian/patches/04_fix_const.patch

--- base/Config.cpp
+++ base/Config.cpp
@@ -417,7 +417,7 @@ void Config::setPaths(char const * const argv0) {
   m_sDataDir = string(EM_DATADIR) + "/";
   m_sExeDir = "./";
   if ( *( m_sDataDir.c_str() ) != '/' ) {
-    char* ptr=0; 
+    const char* ptr=0;
     char* ptrw = 0;
     //cout<<"relative to exe file"<<endl;
     ptr = (strrchr(argv0,'/')); // unix /cygwin / check win32 
