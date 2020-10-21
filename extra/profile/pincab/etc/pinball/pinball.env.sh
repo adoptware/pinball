69a88d493edbd1436d4ff37ef3d1058b5fdcd4d2
Philippe Coval <rzr@users.sf.net>
2020-10-21 11:38:30 +0200

    extra: Add pincab profile
    This reverts commit 2ba06178d83e794edf41fe6fd96a80804d423296.
    
    Change-Id: I3577079745fe9186ea49842bc2218636c9acecf8

---
+++ extra/profile/pincab/etc/pinball/pinball.env.sh
@@ -0,0 +1,43 @@
+ # !/bin/sh
+# XPDX-License-Identifier: GPL-2.0+"
+
+PINBALL_PROFILE="pincab"
+
+PINBALL_BRANCH="next" # TODO
+
+# Output screen
+PINBALL_SCREEN="DVI-I-1"
+
+# TODO: Use preferred resolution (align to /etc/default/pinball)
+PINBALL_RESOLUTION="1024x768"
+
+# TODO
+PINBALL_EXTRA_DIR="/usr/local/opt/pinball"
+
+PINBALL_IMAGE="/usr/local/opt/pinball/src/pinball/tmp/1024x1024/pinball.jpg"
+
+# Random table if defined
+PINBALL_TABLE=
+
+# Quit after displayed frames (for developers)
+# PINBALL_QUIT=4294967295
+# export PINBALL_QUIT
+
+# Readonly FS
+HOME="/tmp"
+
+PINBALL_DISPLAY_MANAGER="xinit"
+DISPLAY=':0'
+XINITRC="/etc/pinball/pinball-session.sh"
+PINBALL_XRANDR_ARGS="--rotate left --mode ${PINBALL_RESOLUTION}"
+
+DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/0/dbus/user_bus_socket'
+# ALSA_CARD=""
+
+# Extra
+# SDL_VIDEODRIVER=dummy
+# SDL_NOMOUSE=
+# SDL_AUDIODRIVER=dummy
+# SDL_DEBUG=
+
+# XINIT_ARGS= -- -nocursor
