c91cf5fcfccc47c81ca38f88a3daa7e2881a3752
Philippe Coval <rzr@users.sf.net>
2020-12-21 00:46:22 +0100

    loader: Reorder config before other includes
    Apparently this patch was applied in FreeBSD tree
    
    Related info:
    
        git-svn-id: https://svn.freebsd.org/ports/head/games/pinball@363362 35697150-7ecd-e111-bb59-0022644237b5
        git-svn-id: https://svn.freebsd.org/ports/head/games/pinball@505292 35697150-7ecd-e111-bb59-0022644237b5
    
    Change-Id: Ic46ef145eafc7a72d9a905a294bada05aad5fc5d
    Origin: https://svnweb.freebsd.org/ports/head/games/pinball/files/patch-src%3A%3ALoader.cpp?revision=96028&view=markup&pathrev=96028
    Author: Thierry Thomas <thierry@pompo.net>
    Signed-off-by: Philippe Coval <rzr@users.sf.net>

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -17,6 +17,7 @@
 #include <iostream>
 
 #include "Private.h"
+#include "Config.h"
 #include "Loader.h"
 #include "Pinball.h"
 #include "Keyboard.h"
@@ -39,7 +40,6 @@
 #include "SoundUtil.h"
 #include "Score.h"
 #include "StdAnimation.h"
-#include "Config.h"
 #include "StateBehavior.h"
 #include "Script.h"
 #include "FakeModuleBehavior.h"
